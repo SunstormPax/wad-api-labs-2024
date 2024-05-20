4ab934f77f7371cf38f2c3bc3b384709848f6803
Sunny Keenan <20097973@mail.wit.ie>
2024-05-20 17:37:56 +0100

    Movie display fixed

--- reactApp/src/api/movies-api.js
+++ reactApp/src/api/movies-api.js
@@ -1,9 +1,15 @@
 export const getMovies = async () => {
-    const response = await  fetch(
-      `https://api.themoviedb.org/3/discover/movie?api_key=4e4633f6f04490f4b0502c039e7297f6&language=en-US&include_adult=false&page=1`
-    )
-    return response.json()
-  };
+  const response = await fetch(
+    'http://localhost:8080/api/movies', {
+    headers: {
+      'Authorization': window.localStorage.getItem('token')
+    }
+  }
+  )
+  return response.json();
+};
+
+
 
   export const login = async (username, password) => {
     const response = await fetch('http://localhost:8080/api/users', {
--- reactApp/src/pages/moviesPage.js
+++ reactApp/src/pages/moviesPage.js
@@ -2,21 +2,33 @@ import { useQuery } from 'react-query';
 import { getMovies } from "../api/movies-api";
 
 const MoviesPage = () => {
-    const { data, error, isLoading, isError } = useQuery('discover', getMovies)
+    const { data, error, isLoading, isError } = useQuery('discover', getMovies);
+
     if (isLoading) {
-        return <h1>Loading...</h1>
+        return <h1>Loading...</h1>;
     }
 
     if (isError) {
-        return <h1>{error.message}</h1>
+        return <h1>{error.message}</h1>;
     }
+
     const movies = data;
     const moviesDisplay = (
         <div>
-            {movies.map(movie => { return <li key={movie.id}>{movie.id},{movie.title}<br /></li> })}
+            {movies.map(movie => (
+                <li key={movie.id}>
+                    {movie.id},{movie.title}<br />
+                </li>
+            ))}
         </div>
-    )
-    return <div><h2>Movies</h2>{moviesDisplay}</div>
+    );
+
+    return (
+        <div>
+            <h2>Movies</h2>
+            {moviesDisplay}
+        </div>
+    );
 }
 
 export default MoviesPage;
