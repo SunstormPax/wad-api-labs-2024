895624a3aa2c210f03b74e1f55d06a46bb593fee
Sunny Keenan <20097973@mail.wit.ie>
2024-05-20 15:43:59 +0100

    Authentication continued

---
+++ reactApp/src/api/movies-api.js
@@ -0,0 +1,28 @@
+export const getMovies = async () => {
+    const response = await  fetch(
+      `https://api.themoviedb.org/3/discover/movie?api_key=4e4633f6f04490f4b0502c039e7297f6&language=en-US&include_adult=false&page=1`
+    )
+    return response.json()
+  };
+
+  export const login = async (username, password) => {
+    const response = await fetch('http://localhost:8080/api/users', {
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        method: 'post',
+        body: JSON.stringify({ username: username, password: password })
+    });
+    return response.json();
+};
+
+export const signup = async (username, password) => {
+    const response = await fetch('http://localhost:8080/api/users?action=register', {
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        method: 'post',
+        body: JSON.stringify({ username: username, password: password })
+    });
+    return response.json();
+};
